c592f5fb30c717c0fba6b095028f6b376dcc9997
jakyastik <12696342+jakyastik@users.noreply.github.com>
2024-07-01 17:47:00 +0530

    added categories to forum

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,2 +1,3 @@
-
-<div class="container-expand-lg  bg-dark"><p class="text-center text-white">Copyright 2024 / The Forum Brand</p></div>
+<div class="footer container-expand-lg  bg-dark">
+    <p class="text-center text-white">Copyright 2024 / The Forum Brand</p>
+</div>
---
+++ partials/_signupmodal.php
@@ -0,0 +1,34 @@
+<!-- Modal -->
+<div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Sign Up</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" id="password">
+                    </div>
+                    <div class="mb-3">
+                        <label for="confirmPass" class="form-label">Password</label>
+                        <input type="password" id="confirmPass" name="confirmPass" class="form-control"
+                            id="confirmPass">
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-success">Sign Up</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
